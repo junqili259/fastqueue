b8a47179db7ad473e92e683154abac43ee199f1a
junqi <junqili259@gmail.com>
2020-03-06 00:27:12 -0500

    Initial Commit

---
+++ FastQueue/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FastQueue
+//
+//  Created by Jun Qi Li on 3/6/20.
+//  Copyright © 2020 Jun Qi Li. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
